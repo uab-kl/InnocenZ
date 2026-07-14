af88ba582c6f2d169d671109fca6bcf78a741f43
Ng8522 <105772971+Ng8522@users.noreply.github.com>
2026-07-14 17:58:35 +0800

    fix: update CI configuration to exclude mobile app builds and improve test rendering

--- apps/mobile/src/app/App.spec.tsx
+++ apps/mobile/src/app/App.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react-native';
 
 import App from './App';
 
-test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
+test('renders correctly', async () => {
+  const { getByTestId } = await render(<App />);
   expect(getByTestId('heading')).toHaveTextContent(/Welcome/);
 });
